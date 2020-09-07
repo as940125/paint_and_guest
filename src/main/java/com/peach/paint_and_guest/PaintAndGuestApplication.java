d07390c2a49a328c71d5b22b3f30b4b9906fd924
as940125 <670954789@qq.com>
2020-09-07 23:53:49 +0800

    init project

---
+++ src/main/java/com/peach/paint_and_guest/PaintAndGuestApplication.java
@@ -0,0 +1,13 @@
+package com.peach.paint_and_guest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintAndGuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaintAndGuestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peach/paint_and_guest/PaintAndGuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peach.paint_and_guest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintAndGuestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
